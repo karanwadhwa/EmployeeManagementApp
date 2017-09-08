32bf3674e2d688952f3dd493e8ce76d7a6c3cd54
Karan Wadhwa <wadhwa.karan96@gmail.com>
2017-09-08 13:54:33 +0530

    initialized firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import firebase from 'firebase';
 
 class App extends React.Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyARH1ycrYYl4WwGigbT5Sbb4UJaG6cEcmo',
+      authDomain: 'employeemanagementapp-a3b0d.firebaseapp.com',
+      databaseURL: 'https://employeemanagementapp-a3b0d.firebaseio.com',
+      projectId: 'employeemanagementapp-a3b0d',
+      storageBucket: '',
+      messagingSenderId: '1067603145811'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View>
